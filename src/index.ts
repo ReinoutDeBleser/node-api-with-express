2828a8192d90034fbe36c621611153b6bff7fb99
RedMarkD <reinout.de.bleser@gmail.com>
2021-12-16 14:40:00 +0100

    Refactor routes - step working

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+import * as sessionAuth from "./middleware/sessionAuth";
+import * as routes from "./routes";
+
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
+
+const app = express();
+
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
+// Configure session auth
+sessionAuth.register( app );
+
+// Configure routes
+routes.register( app );
+
+
+// define a route handler for the default home page
+app.get( "/", ( req, res ) => {
+    // render the index template
+    res.render( "index" );
+} );
+
+// start the express server
+app.listen( port, () => {
+    // tslint:disable-next-line:no-console
+    console.log( `server started at http://localhost:${ port }` );
+} );
