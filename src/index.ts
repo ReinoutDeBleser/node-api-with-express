693c54546d86f16029b0204c68b1033725d5d7ee
RedMarkD <reinout.de.bleser@gmail.com>
2021-12-16 15:41:59 +0100

    removed duplicate code that was causing error

--- src/index.ts
+++ src/index.ts
@@ -9,10 +9,6 @@ app.get( "/", ( req, res ) => {
 
 // start the Express server
 app.listen( port, () => {
-    console.log( `server started at http://localhost:${ port }` );
-} );
-
-app.listen( port, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
 } );
