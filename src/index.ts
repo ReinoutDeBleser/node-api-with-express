e97cc452fd72fdefbf53ee1b11e839a28e3a0fed
RedMarkD <reinout.de.bleser@gmail.com>
2021-12-16 15:57:10 +0100

    added EJS and dependecy for EJS in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
+import path from "path";
 const app = express();
 const port = 8080; // default port to listen
 
+
+// Configure Express to use EJS
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
+    // render the index template
+    res.render( "index" );
 } );
 
 // start the Express server
