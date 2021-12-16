2179e101b4866cef8d64e501b3787d6f59c788fd
RedMarkD <reinout.de.bleser@gmail.com>
2021-12-16 16:07:22 +0100

    added env.sample, added dotenv module and configured dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
+import dotenv from "dotenv";
 import express from "express";
 import path from "path";
-const app = express();
-const port = 8080; // default port to listen
 
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
+
+const app = express();
 
 // Configure Express to use EJS
 app.set( "views", path.join( __dirname, "views" ) );
@@ -14,7 +21,7 @@ app.get( "/", ( req, res ) => {
     res.render( "index" );
 } );
 
-// start the Express server
+// start the express server
 app.listen( port, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
