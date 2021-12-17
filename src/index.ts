380585074814307040b1b669440af4b426841c32
RedMarkD <reinout.de.bleser@gmail.com>
2021-12-17 10:58:04 +0100

    added routes/api.ts and modifies routes/index.ts and src/index.ts to use new api code

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,9 @@ const port = process.env.SERVER_PORT;
 
 const app = express();
 
+// Configure Express to parse incoming JSON data
+app.use( express.json() );
+
 // Configure Express to use EJS
 app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
